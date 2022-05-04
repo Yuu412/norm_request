46957b0d7eea5d6c2b25f74c8b5709fc1271de23
Yuu412 <yuu.yoshi12@outlook.jp>
2022-05-04 18:55:56 +0900

    make ads which is provided by Google Ads

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:flutter/material.dart';
 import 'package:norm_request/app.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -5,6 +6,7 @@ import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
